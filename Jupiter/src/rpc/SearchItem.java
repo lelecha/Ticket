581c0ae37bf184c6fe0d4f6813342c6fa86fac28
Guoqi <maguoqi96@gmail.com>
2020-01-30 14:57:37 -0500

    update SearchItem servlet

--- Jupiter/src/rpc/SearchItem.java
+++ Jupiter/src/rpc/SearchItem.java
@@ -2,17 +2,22 @@ package rpc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.MySQLConnection;
+import entity.Item;
 import external.TicketMasterClient;
 
 /**
@@ -34,13 +39,34 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+
+		String userId = session.getAttribute("user_id").toString();
+
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
-
+		//String userId = request.getParameter("user_id");
 		TicketMasterClient client = new TicketMasterClient();
-		RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
+		List<Item> items = client.search(lat, lon, null);
+		
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			JSONObject obj = item.toJSONObject();
+			try {
+				obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			array.put(obj);
+		}
+		RpcHelper.writeJsonArray(response, array);
 
 	}
 
@@ -48,7 +74,14 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+
+		
+
 		doGet(request, response);
 	}
 
